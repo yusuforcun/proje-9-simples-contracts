5a00ba8bfbc15a190a02d0cfb964057f06f2d9a5
yusuforcun <yusuf.orcun.ozd@gmail.com>
2026-03-28 12:24:37 +0300

    added a lot of

--- contract.sol
+++ contract.sol
@@ -7,7 +7,18 @@ contract userregistry{
         uint age;
     }
 
+    address public owner ;
+
+    event update(address user , string name ,uint age);
+
     mapping(address => user) users;
+
+    mapping(address => user[]) public history ;
+
+    modifier onlyOwner(){
+        require(msg.sender == owner,"Only owner");
+        _;
+    }
 
     function register (string memory name , uint age)public{
         require(users[msg.sender].age==0, "Already registered");
@@ -18,9 +29,31 @@ contract userregistry{
         user memory u = users[msg.sender];
         return(u.name , u.age);
     }
-    function updateData(string memory name , uint age) public{
-        require(users[msg.sender].age != 0 , "Not registered");
-        users[msg.sender] = user(name,age);
+
+    function getMyData2() public view returns(string memory,uint){
+        return(users[msg.sender].name,users[msg.sender].age);
     }
+
+    function updateData(string memory name , uint age) public {
+        require(users[msg.sender].age != 0 , "Not registered");
+        history[msg.sender].push(users[msg.sender]);
+        users[msg.sender] = user(name,age);
+        emit update(msg.sender , name , age);
+    }
+
+    function updateName(string memory name)public {
+        require(users[msg.sender].age != 0,"Not registered");
+        history[msg.sender].push(users[msg.sender]);
+        users[msg.sender].name = name;
+        emit update(msg.sender,name,users[msg.sender].age);
+    }
+
+    function updateAge(uint age)  public {
+        require(users[msg.sender].age != 0,"Not registered");
+        history[msg.sender].push(users[msg.sender]);
+        users[msg.sender].age = age;
+        emit update(msg.sender,users[msg.sender].name,age);
+    }
+
 
 }
