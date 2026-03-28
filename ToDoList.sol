0b1f8998acae89c13bf78fa9fe14c56d34d3b4fd
yusuforcun <yusuf.orcun.ozd@gmail.com>
2026-03-28 13:24:52 +0300

    optimized

--- ToDoList.sol
+++ ToDoList.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.28;
 
 contract ToDoList{
 
+    event TaskAdded(address indexed user,string content );
+    event TaskDeleted(address indexed user , uint index);
+    event TaskToggled(address indexed user , uint index , bool completed);
+
     struct Task{
         string content ;
         bool completed ;
@@ -12,6 +16,7 @@ contract ToDoList{
 
     function addTask(string memory _content)public{
         tasks[msg.sender].push(Task(_content , false)) ;
+        emit TaskAdded(msg.sender, _content);
     }
 
   
@@ -20,6 +25,7 @@ contract ToDoList{
         require(index < tasks[msg.sender].length,"Invalid index");
         Task storage task = tasks[msg.sender][index];
         task.completed = !task.completed;
+        emit TaskToggled(msg.sender, index, task.completed);
     }
 
     function swappopTask(uint index) public{
@@ -27,12 +33,14 @@ contract ToDoList{
         require(index < userTasks.length,"Invalid index");
         userTasks[index] = userTasks[userTasks.length - 1];
         userTasks.pop();
+        emit TaskDeleted(msg.sender, index);
     }
 
     function deleteTask (uint index) public{
         Task[] storage userTasks = tasks[msg.sender];
         require(index < userTasks.length,"Invalid index");
         delete userTasks[index];
+        emit TaskDeleted(msg.sender, index);
     }
 
 }
